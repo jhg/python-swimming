6a1c42d83ae20f77763aaaf2e76eee16fde8d3bd
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-04-22 17:26:36 +0100

    Added prevention of execution as a module to script generate bytecode.

--- src/generate_bytecode.py
+++ src/generate_bytecode.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
+
 import sys
 import py_compile
 
-# Try create a bytecode (pyc) of first argument
-try: py_compile.compile(sys.argv[1])
-except: print 'ERROR: pass a python script as an argument'
+if __name__=='__main__':
+    # Try create a bytecode (pyc) of first argument (only of first)
+    try: py_compile.compile(sys.argv[1])
+    except: print 'ERROR: pass a python script as an argument'
+else:
+    print 'ERROR: Use generate_bytecode as script not is a module'
 
