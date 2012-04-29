3f20d05a1bd9aa5c86b70e8efee1ede6fb998bba
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-04-29 15:30:37 +0100

    Improvement in pure embedding test.

--- test/src/pure_embedding.py
+++ test/src/pure_embedding.py
@@ -8,3 +8,8 @@ def test(a,b):
     print "      Return:        ", a, "+", b, "=", c
     return c
 
+if __name__=='__main__':
+    print('     Ejecutado como programa')
+else:
+    print('     Ejecutado como modulo ' + __name__)
+
